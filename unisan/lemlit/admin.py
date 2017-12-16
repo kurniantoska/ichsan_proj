bd96fc5f306a58bef7a39f33f9232a3aedd4022c
skater <alternative.xen.sakura@gmail.com>
2017-12-16 19:46:40 +0800

    add filter petugas

--- unisan/lemlit/admin.py
+++ unisan/lemlit/admin.py
@@ -5,7 +5,11 @@ PetugasAdmininistrasi,
 StrukturManajemen,
 )
 
+class SuratIzinPenelitianMahasiswaAdmin(admin.ModelAdmin):
+   list_display = ('petugas', 'mahasiswa')
+   search_fields = ['petugas']
+
 # Register your models here.
-admin.site.register(SuratIzinPenelitianMahasiswa)
+admin.site.register(SuratIzinPenelitianMahasiswa, SuratIzinPenelitianMahasiswaAdmin)
 admin.site.register(PetugasAdmininistrasi)
 admin.site.register(StrukturManajemen)
