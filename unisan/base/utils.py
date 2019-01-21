a984689656f77fd1faa28e35fd3fb1cb8af3fc1d
Riska K Abdullah <alternative.xen.sakura@gmail.com>
2019-01-21 12:50:21 +0800

    fix messy tuple

--- unisan/base/utils.py
+++ unisan/base/utils.py
@@ -1,19 +1,25 @@
 from django.core.exceptions import PermissionDenied
 import functools
 
+
 def get_program_studi_full_name(program_studi):
     PROGRAM_STUDI_DICT = {
         'agb': 'Agribisnis',
         'agr': 'Agroteknologi',
+        'thp': 'Teknologi Hasil Pertanian',
+        
         'ak': 'Akuntansi',
+        'man': 'Manajemen',
+
         'hu': 'Ilmu Hukum',
+
         'ko': 'Ilmu Komunikasi',
         'ip': 'Ilmu Pemerintahan',
-        'man': 'Manajemen',
+
         'ar': 'Teknik Arsitektur',
         'el': 'Teknik Elektro',
+
         'ti': 'Teknik Informatika',
-        'thp': 'Teknologi Hasil Pertanian'
     }
     return PROGRAM_STUDI_DICT[program_studi]
 
